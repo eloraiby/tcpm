cabb69db8b78f3152acf5c01f5fa798a1abab0d0
Wael El Oraiby <wael.eloraiby@gmail.com>
2018-07-02 17:15:23 -0400

    Add CMake build files and start implementation

---
+++ tcpm/src/internals.h
@@ -0,0 +1,36 @@
+/*
+    Tiny Cooperative Process Management library
+    Copyright (C) 2018  Wael El Oraiby
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include <pthread.h>
+
+typedef enum {
+    PCT_STOP,
+    PCT_CONTINUE,
+    PCT_WAIT_MESSAGE,
+} ProcessContinuationType;
+
+typedef struct ProcessContinuation ProcessContinuation;
+typedef ProcessContinuation (*ProcessHandler)(DispatcherQueue*, void* localState, void* msg);
+
+struct ProcessContinuation {
+    ProcessContinuationType     ty;
+    void*                       localState;
+    ProcessHandler              handler;
+};
+
